c91ed92839d698d0a2595bc1c9e97a171cbe4449
Tony Marma <45245151+tonymarma@users.noreply.github.com>
2024-03-04 15:14:37 +0600

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,9 +1,9 @@
 import { Colors } from '@chakra-ui/react'
 
 export const colors: Colors = {
-  'light-container': '#FFFFFF',
-  'light-bg': '#F3F4F6',
-  'light-theme': '#00B5D8',
+  'light-container': '#000000',
+  'light-bg': '#FFFF00',
+  'light-theme': '#FFFF00',
   'dark-container': '#2A334C',
   'dark-bg': '#171D30',
   'dark-theme': '#FFFF00',
