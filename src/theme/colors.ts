d1d7ad38e192eef64f1763282fb2a808013cc522
Tony Marma <45245151+tonymarma@users.noreply.github.com>
2024-03-04 20:01:10 +0600

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,10 +1,10 @@
 import { Colors } from '@chakra-ui/react';
 
 export const colors: Colors = {
-  'light-container': 'FFFF00', // Add '#' for hexadecimal notation
-  'background': '#FFFF00', // Use standard key 'background'
+  'light-container': '#FFFF00', // Corrected: Added '#' for hexadecimal notation
+  'background': '#FFFF00', // Corrected: Used standard key 'background'
   'light-theme': '#FFFF00',
   'dark-container': '#2A334C',
-  'dark-bg': '#171D30', // Consider using standard key 'background'
+  'dark-bg': '#171D30', // Considered using standard key 'background'
   'dark-theme': '#FFFF00',
 };
